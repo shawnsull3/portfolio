f05f89f7ee9ef4e26ec8e22d31259543e13415c9
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-19 10:27:45 -0600

    Format file structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Shawn's portfolio
+      </header>
+    </div>
+  );
+}
+
+export default App;
