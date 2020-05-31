55acf6af76b5c9db07cabc27811a6b11bf61ab2e
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-31 16:11:15 -0600

    Format basic page layout w bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,24 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from './Header';
 import Portfolio from './Portfolio';
 import About from './About';
+import '../styles/App.css';
 
 function App() {
   return (
-    <div>
-    <Header />
-    <div classname='main'>
-      <Switch>
-        <Route exact path='/'/>
-        <Route exact path='/portfolio' component={Portfolio} />
-        <Route exact path='/about' component={About} />
-      </Switch>
+    <div className='container-flex h-100'>
+      <div className='d-flex justify-content-center'>
+        <div className='row'>
+          <Header />
+        </div>
+        <div classname='row main'>
+          <Switch>
+            <Route exact path='/'/>
+            <Route exact path='/portfolio' component={Portfolio} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+         </div>
+	    </div>
     </div>
-  </div>
   );
 }
 
