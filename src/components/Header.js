59f60b328487b7f3ccda9931bbe232c570020499
Shawn Sullivan <sullivansm3@gmail.com>
2020-06-03 14:34:04 -0600

    Setup header format and navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
+import '../styles/Header.css';
 
 const Header = () => {
 
     return (
-        <div className='header'>
-          header
+        <div className='row header align-items-center'>
+          <div className='col-4'>Slogan</div>
+          <div className='col-4 justify-content-around'>
+            <Link to='/portfolio' style={{ textDecoration: 'none' }} className='navLink'>
+              Portfolio
+            </Link>
+            <Link to='/about' style={{ textDecoration: 'none' }} className='navLink'>
+              About
+            </Link>
+          </div>
+          <div className='col-4'>Contact info</div>
         </div>
     )
 }
