f2dc0d37198e09102c53fc44ef4d9e0d6981e114
Shawn Sullivan <sullivansm3@gmail.com>
2020-06-23 12:00:57 -0600

    Layout grid format and styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import '../styles/About.css';
 
 const About = () => {
 
     return (
       <div>
-        About
+        <div className='row justify-content-center'>
+          portrait photo place holder
+        </div>
+        <div className='row justify-content-around full-width'>
+          <div className='col-12 col-sm-6 block'>
+            <img className='stateImg' src="https://gisgeography.com/wp-content/uploads/2020/03/Iowa-Outline-Map.jpg" alt="Iowa" /> 
+          </div>
+          <div className='col-12 col-sm-6 block align-self-center'>
+            A whole bunch of text. A whole bunch of text. A whole bunch of text. A whole bunch of text. A whole bunch of text.
+          </div>
+        </div>
+        <div className='row justify-content-around full-width'>
+          <div className='col-12 col-sm-6 block align-self-center'>
+            A whole bunch of text. A whole bunch of text. A whole bunch of text. A whole bunch of text. A whole bunch of text.
+          </div>
+          <div className='col-12 col-sm-6 block'>
+            <img className='stateImg' src="https://gisgeography.com/wp-content/uploads/2020/04/Colorado-Outline-Map.jpg" alt="Colorado" /> 
+          </div>
+        </div>
+        <div className='row justify-content-around full-width'>
+          <div className='col-12 col-sm-6 block'>
+            <img className='stateImg' src="https://gisgeography.com/wp-content/uploads/2020/04/Colorado-Outline-Map.jpg" alt="Colorado" /> 
+          </div>
+          <div className='col-12 col-sm-6 block align-self-center'>
+            A whole bunch of text. A whole bunch of text. A whole bunch of text. A whole bunch of text. A whole bunch of text.
+          </div>
+        </div>
       </div>
     )
 }
