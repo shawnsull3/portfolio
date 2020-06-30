083b3acaa36f65bf67f6c698a3af965a6ebca4e5
Shawn Sullivan <sullivansm3@gmail.com>
2020-06-30 14:39:18 -0600

    Update bootstrap grid for phone sizes

--- src/components/Books/BooksList.js
+++ src/components/Books/BooksList.js
@@ -5,12 +5,12 @@ const BooksList = ({ books }) => {
     <div className='row justify-content-center'>
       {books.map( (book, i) => {
         return (
-            <div className='col-8 entry' key={i} >
+            <div className='col-12 col-sm-10 entry' key={i} >
                 <div className='row'>
-                    <div className='col-3'>
+                    <div className='col-12 col-sm-3'>
                         <img className= 'book-image' src={book.imageURL} alt={book.title} />
                     </div>
-                    <div className='col-9 info'>
+                    <div className='col-12 col-sm-9 info'>
                         <div className='title'>{book.title}</div>
                         <div className='author'>{book.author}</div>     
                     </div>
