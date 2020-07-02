06847c52e8a4df03479efd0d6111bb0041b9cf5b
Shawn Sullivan <sullivansm3@gmail.com>
2020-07-02 16:30:56 -0600

    Import ProjectInfo and set it as a state variable

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
 import PortfolioList from './PortfolioList';
+import projects from './ProjectInfo';
 import '../../styles/Portfolio.css';
-
-import seedspot from './ProjectPictures/seedspot.png';
-import metrics from './ProjectPictures/trackit-metrics.png';
 
 class Portfolio extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        projects: [
-          {
-            title: 'SeedSpot',
-            subtitle: 'Find Quality Investors for Your Startup',
-            description: 'Placeholder text for the time being. Placeholder text for the time being. Placeholder text for the time being.',
-            technologies: ['React', 'Node.js', 'AirTable', `Twilio's SendGrid`],
-            githubLink: 'https://github.com/shawnsull3/seedspot',
-            photos: [seedspot]
-          },
-          {
-            title: 'trackIt',
-            subtitle: 'Track and Improve Your Daily Habits',
-            description: 'Placeholder text for the time being. Placeholder text for the time being. Placeholder text for the time being.',
-            technologies: ['React Native', 'Node.js', 'Express', 'MongoDB'],
-            githubLink: 'https://github.com/shawnsull3/trackit',
-            photos: [metrics]
-          }
-        ]
+        projects: projects
     }
   }
 
@@ -34,8 +15,8 @@ class Portfolio extends Component {
     const { projects } = this.state;
 
     return (
-      <div className='col-12'>
-        <div className='row portfolio'>
+      <div className='col-12 portfolio'>
+        <div className='row'>
           <div className='col-12'>
             <h2 className='portfolio-title'>
               Portfolio
