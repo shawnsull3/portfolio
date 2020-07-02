c8cd0836894f457b76e1dd6d69303b378455c963
Shawn Sullivan <sullivansm3@gmail.com>
2020-07-02 16:32:03 -0600

    Format layout for detailed project info

--- src/components/Portfolio/ProjectDetails.js
+++ src/components/Portfolio/ProjectDetails.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import ProjectInfo from './ProjectInfo';
 
 const ProjectDetails = ({ match }) => {
   const { params: { projectTitle } } = match;
+  const project = ProjectInfo.filter( a => a.title === projectTitle)[0];
   return (
-    <div>
-        {projectTitle}
+    <div className='col-12'>
+        <div className='row justify-content-center'>
+            <div className='col-12 col-lg-10'>
+                <div className='row'>
+                  <h1 className='col-12'>{project.title}</h1>
+                </div>
+
+                <div className='row'>
+                  <h3 className='col-12'>{project.subtitle}</h3>
+                </div>
+
+                <div className='row'>
+                  <div className='col-12 col-md-6'>
+                      <div>
+                      <a href={project.githubLink} className='project-links'>Github</a>
+
+                      </div>
+                      {project.url !== '' && <a href={project.url} className='project-links'>{project.url}</a>}
+                      <div>
+                        <p>{project.overview}</p>   
+                      </div>
+                  </div>
+                  <div className='col-12 col-md-6'>
+                      <div className='row'>
+                        <p className='col-12 tech-text'>Technologies:</p>
+                        {project.technologies.map( (technology, i) => {
+                            return (
+                                <p className='technologies' key={i}>{technology}</p>
+                            )
+                        })}
+                      </div>
+                  </div>
+                </div>
+
+                <div className='row'>
+                  {project.photos.map( (photo, i) => <img src={photo} className='details-images' alt='' key={i}/>)}
+                </div>
+
+            </div>
+        </div>
     </div>
   )
 }
