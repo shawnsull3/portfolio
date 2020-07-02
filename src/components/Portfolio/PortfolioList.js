1413103b64be612168e04fe3224ba4195c9a0f42
Shawn Sullivan <sullivansm3@gmail.com>
2020-07-01 20:09:10 -0600

    Style entries, project images & main background color

--- src/components/Portfolio/PortfolioList.js
+++ src/components/Portfolio/PortfolioList.js
@@ -9,28 +9,32 @@ const PortfolioList = ({ projects }) => {
             <div className='col-12 col-sm-10 entry' key={i} >
                 <div className='row'>
                     <div className='col-12 info'>
-                        <h3 className='project-title'>
-                            <Link to={`/portfolio/${project.title}`}>{project.title}</Link>
+                        <h3>
+                            <Link to={`/portfolio/${project.title}`} className='project-title'>{project.title}</Link>
                         </h3>
                         <h5 className='project-subtitle'>{project.subtitle}</h5> 
                         <p>{project.description}</p>    
                     </div>
-                    <div className='col-12'>
-                        <Link to={`/portfolio/${project.title}`}>
-                            <img className= 'project-image' 
-                                src={project.photos[0]} 
-                                alt={project.title} 
-                            />
-                        </Link>
+                    <div className='row'>
+                        <div className='col-12 col-sm-8'>
+                            <Link to={`/portfolio/${project.title}`}>
+                                <img className= 'project-image' 
+                                    src={project.photos[0]} 
+                                    alt={project.title} 
+                                />
+                            </Link>
+                        </div>
+                        <div className='col-12 col-sm-4'>
+                            <div className='row'>
+                                <p className='col-12 tech-text'>Technologies:</p>
+                                {project.technologies.map( (technology, i) => {
+                                    return (
+                                        <p className='technologies' key={i}>{technology}</p>
+                                    )
+                                })}
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className='row'>
-                    <p className='col-12 tech-text'>Technologies:</p>
-                    {project.technologies.map( (technology, i) => {
-                        return (
-                            <p className='technologies' key={i}>{technology}</p>
-                        )
-                    })}
                 </div>
             </div>
         )
