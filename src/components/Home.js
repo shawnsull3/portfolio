84b0365e55a4d57c9232305cd94fb9ed68adf5f4
Shawn Sullivan <sullivansm3@gmail.com>
2020-07-04 15:27:23 -0600

    Create Home comp, render and style random quotes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import quotes from './common/quotes';
+import '../styles/Home.css';
 
 const Home = () => {
+    const randomIndex = Math.floor(Math.random() * ((quotes.length-1) - 0 + 1) + 0);
     return (
-        <div>Home</div>
+        <div className='col-12'>
+          <div className='row justify-content-center'>
+            <div className='col-12 col-sm-6 text-center'>
+              <p className='quote'>{quotes[randomIndex]}</p>
+            </div>
+          </div>
+
+        </div>
     )
 }
 
