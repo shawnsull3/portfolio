160f0ae3e897526cf2f0aa39bb701bbeb45e1946
Shawn Sullivan <sullivansm3@gmail.com>
2020-07-04 15:26:45 -0600

    Export an array of quotes

---
+++ src/components/common/quotes.js
@@ -0,0 +1,27 @@
+const quotes = [
+    `“Desire is suffering. Every desire you have is an axis where you will suffer. So just don’t focus on more than one desire to time." - Naval Ravikant`,
+    `"Specialization is for insects.” - Robert A. Heinlein`,
+    `“The ultimate purpose of life, mind, and human striving: to deploy energy and information to fight back the tide of entropy and carve out refuges of beneficial order.” - Steven Pinker`,
+    `"When you learn how to be curious, you create an arc of learning that continues for the rest of your life" - Neil deGrasse Tyson`,
+    `“It doesn’t matter how pretty your theory is, if it doesn’t fit with experiment it’s wrong” - Richard Feynman`,
+    `"A Failure is Just One Step Closer in the Right Direction" - Safi Bahcall`,
+    `“A genius is the one most like himself.” - Thelonius Monk`,
+    `"But no matter the medicinal virtues of being a true friend or sustaining a long close relationship with another, the ultimate touchstone of friendship is not improvement, neither of the other nor of the self; the ultimate touchstone is witness, the privilege of having been seen by someone and the equal privilege of being granted the sight of the essence of another, to have walked with them and to have believed in them, and sometimes just to have accompanied them for however brief a span, on a journey impossible to accomplish alone.” - David Whyte`,
+    `“It is the fate — the genetic and neural fate — of every human being to be a unique individual, to find his own path, to live his own life, to die his own death.” - Oliver Sacks`,
+    `“You have no responsibility to live up to what other people think you ought to accomplish. I have no responsibility to be like they expect me to be. It's their mistake, not my failing.” - Richard Feynman`,
+    `“Every day of our lives, we are on the verge of making those slight changes that would make all the difference.”  - Mignon McLaughlin`,
+    `"Not one of your pertinent ancestors was squashed, devoured, drowned, starved, stranded, stuck fast, untimely wounded, or otherwise deflected from its life's quest of delivering a tiny charge of genetic material to the right partner at the right moment in order to perpetuate the only possible sequence of hereditary combinations that could result — eventually, astoundingly, and all too briefly — in you." - Bill Bryson`,
+    `"Everything can be taken from a man but one thing: the last of the human freedoms — to choose one’s attitude in any given set of circumstances, to choose one’s own way" - ViKtor Frankl`,
+    `"A playful mind is inquisitive, and learning is fun. If you indulge your natural curiosity and retain a sense of fun in new experience, I think you'll find it functions as a sort of shock absorber for the bumpy road ahead." - Bill Watterson`,
+    `“I don’t believe you should do what you’re passionate about; I believe you should be passionate about what you do. Waiting for passion to show up means waiting for fear to go away, and fear is never going to go away.” - Seth Godin`,
+    `"When you're so sure you're right that you're willing to burn things down, it turns out that everyone is standing in a burning building sooner or later" - Seth Godin`,
+    `“The truth is, we know so little about life, we don’t really know what the good news is and what the bad news is” - Kurt Vonnegut`,
+    `"Life shrinks or expands in proportion to one's courage" - Anais Nin`,
+    `“If we can forgive what's been done to us... If we can forgive what we've done to others... If we can leave our stories behind. Our being victims and villains. Only then can we maybe rescue the world.” — Chuck Palahniuk`,
+    `"What you allow is what will continue."`,
+    `“A society grows great when old men plant trees, the shade of which they know they’ll never sit in.”`,
+    `"It's the truth I'm after, and the truth never harmed anyone. What harms us is to persist in self-deceit and ignorance." - Marcus Aurelius`,
+    `“If it is peace you want, seek to change yourself, not other people. It is easier to protect your feet with slippers than to carpet the whole of the earth.” — Anthony de Mello`,
+]
+
+export default quotes;
