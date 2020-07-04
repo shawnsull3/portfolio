eaf27de95b9abf152e6a116586acd6e2079472b6
Shawn Sullivan <sullivansm3@gmail.com>
2020-07-04 16:23:42 -0600

    Add quick description of myself

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,26 @@ const Home = () => {
     return (
         <div className='col-12'>
           <div className='row justify-content-center'>
-            <div className='col-12 col-sm-6 text-center'>
+            <div className='col-12 col-sm-8 text-center'>
               <p className='quote'>{quotes[randomIndex]}</p>
+            </div>
+          </div>
+
+          <div className='row justify-content-center'>
+            <div className='col-12 col-sm-8'>
+                <h4 className='text-center'>Me in 20 seconds</h4>
+                I am a..
+                <ul>
+                    <li>Convert software developer from the chemical engineering realm, who’s currently looking for a project to contribute my software skills to.</li>
+                    <li>Generalist who enjoys damn near everything: reading, biking, coding, hiking, music, cooking, and the list goes on ad infinitum.</li>
+                    <li>Self-proclaimed nerd, problem solver, amateur musician, and clumsy adventurer.</li>
+                </ul>
+            </div>
+          </div>
+
+          <div className='row justify-content-center'>
+            <div className='col-12 col-sm-8 text-center'>
+              <h4>Skills</h4>
             </div>
           </div>
 
